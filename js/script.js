dfd2b98c979f11b19d98a3268cacf079616c33e4
Javin Li <javin.li@gmail.com>
2015-07-03 13:48:42 +0100

    Simple calculator
    Quick calculator application using JS

---
+++ js/script.js
@@ -0,0 +1,20 @@
+function add(){
+	tmp_num1 = parseInt(document.getElementById("var1").value);
+	tmp_num2 = parseInt(document.getElementById("var2").value);
+	alert(tmp_num1 + tmp_num2);
+}
+function subtract(){
+	tmp_num1 = parseInt(document.getElementById("var1").value);
+	tmp_num2 = parseInt(document.getElementById("var2").value);
+	alert(tmp_num1 - tmp_num2);
+}
+function multiply(){
+	tmp_num1 = parseInt(document.getElementById("var1").value);
+	tmp_num2 = parseInt(document.getElementById("var2").value);
+	alert(tmp_num1 * tmp_num2);
+}
+function divide(){
+	tmp_num1 = parseInt(document.getElementById("var1").value);
+	tmp_num2 = parseInt(document.getElementById("var2").value);
+	alert(tmp_num1 / tmp_num2);
+}
